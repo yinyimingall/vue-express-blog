481f6cc1449750cdcc1139f61383379e661bb348
yinyimingall <673275694@qq.com>
2017-07-10 23:25:52 +0800

    添加axios，完成登录注册雏形

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'base',
+      component: Login
     },
     {
       path: '/login',
